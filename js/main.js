66ae23d02cef28a799cf5d78d5beeed2f609f6d8
Yaroslav Grigoryev <yaroslavgrigoryev@gmail.com>
2024-06-21 21:43:07 +0500

    v2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
-  let showClass = 'show_mobile_menu'
-  let container = $('.header_top_right')
+  const showClass = 'show_mobile_menu'
+  const container = $('.header_top_right')
   $('.mobile_menu_button .items').on('click', function() {
     if (container.hasClass(showClass)) {
       container.removeClass(showClass)
