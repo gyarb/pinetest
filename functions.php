b446f2c64d8bbab48a0ec3d41b00cc513379b34f
Yaroslav Grigoryev <yaroslavgrigoryev@gmail.com>
2024-06-23 11:54:24 +0500

    v4

--- functions.php
+++ functions.php
@@ -10,9 +10,8 @@ function p_a($arr)
 add_action('wp_enqueue_scripts', 'theme_enqueue_fn');
 function theme_enqueue_fn()
 {
-  $ver = '1.0.0';
-  wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/styles/css/main.css', $ver, 'all');
-  wp_enqueue_script('theme-script', get_stylesheet_directory_uri() . '/js/main.js', array('jquery'), $ver, true);
+  wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/styles/css/main.css', array(), filemtime(get_theme_file_path('/styles/css/main.css')));
+  wp_enqueue_script('theme-script', get_stylesheet_directory_uri() . '/js/main.js', array('jquery'), filemtime(get_theme_file_path('/js/main.js')), true);
 }
 
 add_action('after_setup_theme', function () {
@@ -35,9 +34,9 @@ function my_acf_blocks_init()
       'mode'              => 'preview',
       'render_template'   => get_stylesheet_directory() . '/parts/blocks/slider/slider.php',
       'enqueue_assets' => function () {
-        wp_enqueue_style('block-slider', get_stylesheet_directory_uri() . '/parts/blocks/slider/styles/css/style.css');
+        wp_enqueue_style('block-slider', get_stylesheet_directory_uri() . '/parts/blocks/slider/styles/css/style.css', array(), filemtime(get_theme_file_path('/parts/blocks/slider/styles/css/style.css')));
         wp_enqueue_script('slick-min', get_stylesheet_directory_uri() . '/parts/blocks/slider/slick/slick.min.js', array(), '', true);
-        wp_enqueue_script('block-image-text', get_stylesheet_directory_uri() . '/parts/blocks/slider/js/script.js', array('jquery', 'slick-min'), '', true);
+        wp_enqueue_script('block-image-text', get_stylesheet_directory_uri() . '/parts/blocks/slider/js/script.js', array('jquery', 'slick-min'), filemtime(get_theme_file_path('/parts/blocks/slider/js/script.js')), true);
       },
       'example'           => array(
         'attributes' => array(
@@ -58,8 +57,10 @@ function my_acf_blocks_init()
       'category'          => 'design',
       'mode'              => 'preview',
       'render_template'   => get_stylesheet_directory() . '/parts/blocks/image-text/image-text.php',
-      'enqueue_style'     => get_stylesheet_directory_uri() . '/parts/blocks/image-text/styles/css/style.css',
-      'enqueue_script'    => get_stylesheet_directory_uri() . '/parts/blocks/image-text/js/script.js',
+      'enqueue_assets' => function () {
+        wp_enqueue_style('block-image-text', get_stylesheet_directory_uri() . '/parts/blocks/image-text/styles/css/style.css', array(), filemtime(get_theme_file_path('/parts/blocks/image-text/styles/css/style.css')));
+        wp_enqueue_script('block-image-text', get_stylesheet_directory_uri() . '/parts/blocks/image-text/js/script.js', array('jquery'), filemtime(get_theme_file_path('/parts/blocks/image-text/js/script.js')), true);
+      },
       'example'           => array(
         'attributes' => array(
           'mode' => 'preview',
@@ -80,9 +81,9 @@ function my_acf_blocks_init()
       'mode'              => 'preview',
       'render_template'   => get_stylesheet_directory() . '/parts/blocks/accordion/accordion.php',
       'enqueue_assets' => function () {
-        wp_enqueue_style('block-accordion', get_stylesheet_directory_uri() . '/parts/blocks/accordion/styles/css/style.css');
+        wp_enqueue_style('block-accordion', get_stylesheet_directory_uri() . '/parts/blocks/accordion/styles/css/style.css', array(), filemtime(get_theme_file_path('/parts/blocks/accordion/styles/css/style.css')));
         wp_enqueue_script('jquery-ui', '//code.jquery.com/ui/1.13.3/jquery-ui.js', array(), '', true);
-        wp_enqueue_script('block-accordion', get_stylesheet_directory_uri() . '/parts/blocks/accordion/js/script.js', array('jquery', 'jquery-ui'), '', true);
+        wp_enqueue_script('block-accordion', get_stylesheet_directory_uri() . '/parts/blocks/accordion/js/script.js', array('jquery', 'jquery-ui'), filemtime(get_theme_file_path('/parts/blocks/accordion/js/script.js')), true);
       },
       'example' => array(
         'attributes' => array(
